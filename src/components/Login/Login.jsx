157428aa676edec07af2cddd85bb89e82d60f70c
Sheikh Jabed <jabedbd2295@gmail.com>
2024-11-09 14:15:45 +0600

    Login Authentication Completed

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,24 +1,36 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth/web-extension";
+
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import auth from "../../firebase/firebase.init";
+import { useState } from "react";
 
 
 const Login = () => {
+    const [user, setUser] = useState(null);
     const provider = new GoogleAuthProvider();
 
     const handleGoogleSignIn = () => {
         signInWithPopup(auth, provider)
         .then ((result) => {
-            console.log(result);
+            console.log(result.user);
+            setUser(result.user)
         })
         .catch(error => {
-            console.log("Error", error);
+            console.log("ERROR", error);
+            setUser(null);
         })
     }
 
     return (
-        <div>
-            <button onClick={handleGoogleSignIn}>Login with Google</button>
-        </div>
+      <div>
+        <button onClick={handleGoogleSignIn}>Login with Google</button>
+        {user && (
+          <div>
+            <h3>{user.displayName}</h3>
+            <p>Email: {user.email}</p>
+            <img src={user.photoURL}></img>
+          </div>
+        )}
+      </div>
     );
 };
 
--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -1,8 +1,8 @@
 // We should not store config on the client side
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth/web-extension";
+import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,7 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 const auth = getAuth(app);
 
 export default auth;
